c38fa0e3c0269f7ae1cf41910f076a277c40c58b
rankangan <rankangan@wps.cn>
2023-05-20 17:41:05 +0800

    update

---
+++ packages/components/no-ssr/index.tsx
@@ -0,0 +1,47 @@
+import React, { Component, FC, ReactNode, useEffect, useState } from 'react'
+
+export interface NoSSRProps {
+  children: ReactNode
+  onSSR?: ReactNode
+}
+
+const DefaultOnSSR = () => <></>
+
+export class ClassNoSSR extends Component<NoSSRProps> {
+  constructor(props: NoSSRProps) {
+    super(props)
+  }
+  state = {
+    canRender: false,
+  }
+
+  componentDidMount() {}
+
+  render() {
+    const { onSSR, children } = this.props
+    const OnSSR = onSSR ? () => <>{onSSR}</> : DefaultOnSSR
+    const OnClient = () => <>{children}</>
+
+    if (this.state.canRender) {
+      return <OnClient />
+    }
+
+    return <OnSSR />
+  }
+}
+
+export const HooksNoSSR: FC<NoSSRProps> = (props) => {
+  const { children, onSSR } = props
+  const [canRender, setCanrender] = useState(false)
+  const OnSSR = onSSR ? () => <>{onSSR}</> : DefaultOnSSR
+
+  const OnClient = () => <>{children}</>
+  useEffect(() => {
+    !canRender && setCanrender(true)
+  }, [canRender])
+
+  if (canRender) {
+    return <OnClient />
+  }
+  return <OnSSR />
+}
